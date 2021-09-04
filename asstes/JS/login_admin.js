76bb9e28754b69d64379421748613b8fb33f0e3b
mdkhaledhossain <khaled.hossain05@northsouth.edu>
2021-09-04 18:29:25 +0600

    New update by khaled

---
+++ asstes/JS/login_admin.js
@@ -0,0 +1,14 @@
+function validateFrom(){
+    var x = document.forms["login_admin_form"] ["email"].value;
+    var y = document.forms["login_admin_form"] ["pass"].value;
+
+    if(x == ""){
+        alert(" Email can't be empty");
+        return false;
+    }
+
+    if(y == ""){
+        alert("Passwoard can't be empty");
+        return false;
+    }
+}
